49d5224af18bf6e57bda753cd5f01f8f312d6428
IKHUpy <72260774+IKHUpy@users.noreply.github.com>
2024-02-28 23:31:37 +0800

    ;

--- Assets/Scripting/PlayerController.cs
+++ Assets/Scripting/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,8 +10,11 @@ public class PlayerController : MonoBehaviour {
     private float moveSpeed = 5f;
     private bool is_moving;
     private Vector2 input;
-
+    private Animator animator;
     private PlayerObject instPlayerObject;
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
     void Start() {
         instPlayerObject = FindObjectOfType<PlayerObject>();
     }
@@ -19,33 +23,28 @@ public class PlayerController : MonoBehaviour {
         if (!is_moving) {
             input.x = Input.GetAxis("Horizontal");
             input.y = Input.GetAxis("Vertical");
+
+            animator.SetFloat("Blend", Mathf.Abs(input.x) + Mathf.Abs(input.y));
             if (input != Vector2.zero) {
                 var target_position = transform.position;
                 target_position.x += input.x;
                 target_position.y += input.y;
+                RotateTowards(target_position);
                 StartCoroutine(Move(target_position));
             }
 
         }
-        //horizontalInput = Input.GetAxis("Horizontal");
-        //verticalInput = Input.GetAxis("Vertical");
-        //
-        //if (horizontalInput != 0 || verticalInput != 0) {
-        //    Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0f).normalized;
-        //
-        //    Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, moveDirection);
-        //    targetRotation = Quaternion.Euler(0, 0, targetRotation.eulerAngles.z); // Keep the z-axis rotation constant
-        //
-        //    instPlayerObject.Move(moveDirection, targetRotation, new Vector2(horizontalInput, verticalInput));
-        //}
     }
+    private void RotateTowards(Vector3 targetPosition) {
+        Vector3 direction = targetPosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
     IEnumerator Move(Vector3 target_position) {
         is_moving = true;
         while ((target_position - transform.position).sqrMagnitude > Mathf.Epsilon) {
             transform.position = Vector3.MoveTowards(transform.position, target_position, moveSpeed * Time.deltaTime);
-            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, target_position);
-            targetRotation = Quaternion.Euler(0, 0, targetRotation.eulerAngles.z);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 5000000f * Time.deltaTime);
             yield return null;
         }
         transform.position = target_position;
